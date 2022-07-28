aeb67b89962457f0bd4dd8f48e451b3c85659e79
MattyModz <63148923+MattyModz@users.noreply.github.com>
2022-07-28 07:21:46 +0100

    link resolver url

--- mycontentpal/resolveProductionUrl.js
+++ mycontentpal/resolveProductionUrl.js
@@ -2,28 +2,19 @@
 const SANITY_PREVIEW_SECRET = "JZSLlav7SIse2qG";
 
 // Replace `remoteUrl` with your deployed Next.js site
-const remoteUrl = `https://mcp-rust.vercel.app/`;
-const localUrl = `http://localhost:3000/post`;
+const remoteUrl = `https://mcp-rust.vercel.app`;
+const localUrl = `http://localhost:3000`;
+const baseUrl = window.location.hostname === "localhost" ? localUrl : remoteUrl;
+
+const previewUrl = new URL(baseUrl);
 
 export default function resolveProductionUrl(doc) {
   switch (doc._type) {
     case "post":
-      return `http://mcp-rust.vercel.app/api/preview?secret=${SANITY_PREVIEW_SECRET}&slug=post/${doc.slug.current}`;
+      return `${previewUrl}/api/preview?secret=${SANITY_PREVIEW_SECRET}&slug=post/${doc.slug.current}`;
   }
   switch (doc._type) {
     case "policy":
-      return `http://mcp-rust.vercel.app/api/preview?secret=${SANITY_PREVIEW_SECRET}&slug=policy/${doc.slug.current}`;
+      return `${previewUrl}/api/preview?secret=${SANITY_PREVIEW_SECRET}&slug=policy/${doc.slug.current}`;
   }
 }
-
-// `/post/${doc?.slug?.current}`;
-// const baseUrl = remoteUrl;
-// // window.location.hostname === "localhost" ? localUrl : remoteUrl;
-
-// const previewUrl = new URL(baseUrl);
-
-// previewUrl.pathname = `/api/preview`;
-// previewUrl.searchParams.append(`secret`, SANITY_PREVIEW_SECRET);
-// previewUrl.searchParams.append(`slug`, doc?.slug?.current ?? `/`);
-
-// return previewUrl.toString();
